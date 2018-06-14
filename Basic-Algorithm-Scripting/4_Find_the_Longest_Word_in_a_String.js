f21d8fe064460e0979ff683d6941519b9853a113
Joe Warren <joe.warren7@gmail.com>
2018-06-13 22:13:06 -0500

    Solves 4_Find_the_Longest_Word_in_a_String

--- Basic-Algorithm-Scripting/4_Find_the_Longest_Word_in_a_String.js
+++ Basic-Algorithm-Scripting/4_Find_the_Longest_Word_in_a_String.js
@@ -1,3 +1,19 @@
 // Return the length of the longest word in the provided sentence.
-
 // Your response should be a number.
+
+const findLongestWordLength = str => {
+    // turn string into array
+    const arr = str.split(' ');
+    console.log(`arr: ${arr}`);
+
+    //create variable for longest word
+    let longestWord = 0;
+
+    //map through array and reassign longestWord to the longest word
+    arr.map(word => word.length > longestWord ? longestWord = word.length : longestWord);
+
+    //return length of longest word
+    return longestWord
+}
+
+findLongestWordLength("What is the average airspeed velocity of an unladen swallow");
