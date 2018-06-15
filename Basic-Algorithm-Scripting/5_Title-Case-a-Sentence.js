f62d3e86612407a3167452019c13038fdc81ffad
Joe Warren <joe.warren7@gmail.com>
2018-06-15 15:31:55 -0500

    Solves 5_Title-Case-a-Sentence

--- Basic-Algorithm-Scripting/5_Title-Case-a-Sentence.js
+++ Basic-Algorithm-Scripting/5_Title-Case-a-Sentence.js
@@ -1,3 +1,13 @@
 // Return the provided string with the first letter of each word capitalized.Make sure the rest of the word is in lower case.
-
 // For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+
+const titleCase = str => {
+    // make each word lowercase and split str into an array
+    const arr = str.toLowerCase().split(' ');
+    console.log(`arr: ${arr}`);
+
+    // loop through and make 0 index toUpperCase()
+    const newArr = arr.map(word => word[0].toUpperCase() + word.slice(1));
+    return newArr.join(' ');
+}
+titleCase('makE suRe the Rest of the WorD is in lower Case.')
