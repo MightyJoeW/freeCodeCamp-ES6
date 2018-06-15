748e3abcac18229fe41f84bfc4c2a46b0a05bf58
Joe Warren <joe.warren7@gmail.com>
2018-06-14 23:57:30 -0500

    Moves answer 3 to correct file

--- Basic-Algorithm-Scripting/3_Check-for-Palindromes.js
+++ Basic-Algorithm-Scripting/3_Check-for-Palindromes.js
@@ -9,3 +9,11 @@
 
 // We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
 
+const palindrome = word => {
+    const regWord = /[^A-Za-z0-9]/g;
+    const lwrCase = word.toLowerCase().replace(regWord, '');
+    const reversed = lwrCase.split('').reverse().join('')
+
+    return reversed === lwrCase;
+}
+palindrome('Racecar');
