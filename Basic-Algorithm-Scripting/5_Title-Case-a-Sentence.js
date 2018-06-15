4a3f30c698c6785de06d27ff3fb09f7c56510058
Joe Warren <joe.warren7@gmail.com>
2018-06-14 23:52:36 -0500

    Solves 3_Check-for-Palindromes

--- Basic-Algorithm-Scripting/5_Title-Case-a-Sentence.js
+++ Basic-Algorithm-Scripting/5_Title-Case-a-Sentence.js
@@ -1,3 +1,12 @@
 // Return the provided string with the first letter of each word capitalized.Make sure the rest of the word is in lower case.
 
 // For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+
+const palindrome = word => {
+    const regWord = /[^A-Za-z0-9]/g;
+    const lwrCase = word.toLowerCase().replace(regWord, '');
+    const reversed = lwrCase.split('').reverse().join('')
+
+    return reversed === lwrCase;
+}
+palindrome('Racecar');
