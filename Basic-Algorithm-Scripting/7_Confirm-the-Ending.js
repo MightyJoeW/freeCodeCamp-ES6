47cd632dda9b515b7bc45d4d3aa0cb2af5100ea9
Joe Warren <joe.warren7@gmail.com>
2018-06-19 21:25:37 -0500

    Solves 7_Confirm-the-Ending

--- Basic-Algorithm-Scripting/7_Confirm-the-Ending.js
+++ Basic-Algorithm-Scripting/7_Confirm-the-Ending.js
@@ -3,12 +3,9 @@
 // This challenge can be solved with the.endsWith() method, which was introduced in ES2015.But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
 
 const confirmEnding = (str, target) => {
-    // "Never give up and good luck will find you."
-    // -- Falcor
-    return str;
+    return str.substr(-target.length) === target;
 }
-
-confirmEnding("Bastian", "n");
+confirmEnding("Abstraction", "action")
 
 // confirmEnding("Bastian", "n") should return true.
 // confirmEnding("Congratulation", "on") should return true.
