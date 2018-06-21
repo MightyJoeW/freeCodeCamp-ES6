e12de0ea9ba6c7278eb49ca08d7327839abe126f
Joe Warren <joe.warren7@gmail.com>
2018-06-20 20:52:10 -0500

    Solves 9_Truncate-a-string

--- Basic-Algorithm-Scripting/9_Truncate-a-string.js
+++ Basic-Algorithm-Scripting/9_Truncate-a-string.js
@@ -1,1 +1,10 @@
-// Truncate a string(first argument) if it is longer than the given maximum string length(second argument).Return the truncated string with a ...ending.
+// // Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+// Return the truncated string with a ... ending.
+
+const truncateString = (str, num) => {
+    return str.length > num
+        ? `${str.slice(0, num)}...`
+        : str
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length)
