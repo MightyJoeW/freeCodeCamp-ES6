12cec35ebcb250a1ab7357afae0f054656a40dec
Joe Warren <joe.warren7@gmail.com>
2018-06-23 23:51:26 -0500

    Starts Slasher Flick (internet down, can't see original problem)

--- Basic-Algorithm-Scripting/11_Slasher-Flick.js
+++ Basic-Algorithm-Scripting/11_Slasher-Flick.js
@@ -1,3 +1,8 @@
 // Return the remaining elements of an array after chopping off n elements from the head.
 
 // The head means the beginning of the array, or the zeroth index.
+
+const slasher = (str, num) => {
+    return `${str} {num}`;
+}
+slasher("Internet is down", 3)
