fea403b0821fbdd15bf8b94e852dd85a4a0254c2
Joe Warren <joe.warren7@gmail.com>
2018-06-26 23:57:02 -0500

    12_Mutations intial attempt

--- Basic-Algorithm-Scripting/12_Mutations.js
+++ Basic-Algorithm-Scripting/12_Mutations.js
@@ -5,3 +5,26 @@
 // The arguments["hello", "hey"] should return false because the string "hello" does not contain a "y".
 
 //     Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+
+function mutation(arr) {
+    const str1 = arr[0].toLowerCase();
+    console.log({ str1 });
+
+    const str2 = arr[1].toLowerCase();
+    console.log({ str2 });
+
+    return str1.contains(str2);
+}
+
+mutation(["Alien", "line"])
+
+// Solution
+// function mutation(arr) {
+//     var tempVal = true;
+//     for (var i = 0; i < arr[1].length; i++) {
+//         if (arr[0].toLowerCase().indexOf(arr[1][i].toLowerCase()) == -1) {
+//             tempVal = false;
+//         }
+//     }
+//     return tempVal;
+// }
