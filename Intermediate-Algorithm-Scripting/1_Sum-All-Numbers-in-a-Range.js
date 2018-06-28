2c851c57024568e29bb63bdbb7af445a201a5963
Joe Warren <joe.warren7@gmail.com>
2018-06-27 23:29:19 -0500

    Started 1_Sum-All-Numbers-in-a-Range

--- Intermediate-Algorithm-Scripting/1_Sum-All-Numbers-in-a-Range.js
+++ Intermediate-Algorithm-Scripting/1_Sum-All-Numbers-in-a-Range.js
@@ -1,3 +1,10 @@
 // We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
 
 // The lowest number will not always come first.
+
+function sumAll(arr) {
+    const reducer = (acc, cur) => acc + cur;
+    console.log(arr.reduce(reducer));
+}
+
+sumAll([1, 4]);
